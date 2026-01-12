e7a2baafee4e061d8f337ecd471bda9768bae935
gennadykudryavtsev <gennadykudryavtsev@gmail.com>
2026-01-12 21:57:10 +0300

    firstSpringApplication

--- src/main/java/com/kudryavtsevgennady/spring/App.java
+++ src/main/java/com/kudryavtsevgennady/spring/App.java
@@ -1,13 +1,15 @@
 package com.kudryavtsevgennady.spring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+
     }
 }
